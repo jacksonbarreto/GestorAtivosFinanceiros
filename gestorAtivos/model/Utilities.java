deb3089bb609854b0a0be03684093ba2a7b91731
Jackson Barreto <jackson.barreto@gmail.com>
2021-01-02 14:22:05 +0000

    implemented the method that returns the password hash.

--- gestorAtivos/model/Utilities.java
+++ gestorAtivos/model/Utilities.java
@@ -1,6 +1,11 @@
 package model;
 
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
 import java.time.LocalDate;
 
 final public class Utilities {
@@ -22,10 +27,30 @@ final public class Utilities {
         );
     }
 
-    static public byte[] getSalt(){
+    static public byte[] getSaltRandom(){
         SecureRandom random = new SecureRandom();
         byte[] salt = new byte[16];
         random.nextBytes(salt);
         return salt;
+    }
+
+    static public String getHashedPassword(String password, byte[] salt){
+        String hexHashedPassword = null;
+        StringBuilder hexString = new StringBuilder();
+        KeySpec spec = new PBEKeySpec(password.toCharArray(), salt, 65536, 128);
+        SecretKeyFactory factory;
+
+        try {
+            factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+            byte[] hashedPassword = factory.generateSecret(spec).getEncoded();
+            for(byte b : hashedPassword){
+                hexString.append(String.format("%02X", 0xFF & b));
+            }
+            hexHashedPassword = hexString.toString();
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            e.printStackTrace();
+        }
+
+        return hexHashedPassword;
     }
 }
