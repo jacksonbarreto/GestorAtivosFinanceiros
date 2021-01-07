e2bd52f7e269b7427dbe6795af0a4c9b07901cb4
Jackson Barreto <jackson.barreto@gmail.com>
2021-01-07 09:52:37 +0000

    Implemented the close button on the login screen

--- gestorAtivos/controller/LoginController.java
+++ gestorAtivos/controller/LoginController.java
@@ -5,6 +5,8 @@ import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.shape.Circle;
 
 import java.awt.event.ActionEvent;
 import java.net.URL;
@@ -19,6 +21,9 @@ public class LoginController implements Initializable {
     private JFXPasswordField inpPassword;
 
     @FXML
+    private Circle btnClose;
+
+    @FXML
     private JFXButton btnLogIn;
 
     @Override
@@ -30,5 +35,11 @@ public class LoginController implements Initializable {
     public void handleLogIn(javafx.event.ActionEvent actionEvent) {
         System.out.println(inpUsername.getText());
         System.out.println(inpPassword.getText());
+    }
+
+    public void handleMouseEvent(MouseEvent mouseEvent) {
+        if (mouseEvent.getSource() == btnClose){
+            System.exit(0);
+        }
     }
 }
