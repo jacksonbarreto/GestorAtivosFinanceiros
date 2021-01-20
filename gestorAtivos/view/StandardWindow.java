eebb0e3c15808c9f8c3c9ba6d592b323e7e3f288
Jackson Barreto <jackson.barreto@gmail.com>
2021-01-20 22:27:37 +0000

    fixed problems in the transition of screens (login to main).

---
+++ gestorAtivos/view/StandardWindow.java
@@ -0,0 +1,13 @@
+package view;
+
+import javafx.stage.Stage;
+
+public interface StandardWindow {
+
+    static Stage getStage() {
+        return null;
+    }
+
+    static void setStage(Stage stage) {
+    }
+}
---
+++ gestorAtivos/view/Utilities.java
@@ -0,0 +1,42 @@
+package view;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+
+public class Utilities {
+
+    public static Parent configureWindow(String resource, Stage primaryStage, Class<?> classe) throws IOException {
+        if (resource == null || resource.isEmpty() || primaryStage == null || classe == null || !implementsStandardWindow(classe))
+            throw new IllegalArgumentException();
+        Parent root = FXMLLoader.load(classe.getResource(resource));
+        Scene scene = new Scene(root);
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
+        scene.setFill(Color.TRANSPARENT);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        try {
+            classe.getMethod("setStage", Stage.class).invoke(classe, primaryStage);
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        return root;
+    }
+
+    private static boolean implementsStandardWindow(Class<?> classe) {
+        Class<?>[] interfaces = classe.getInterfaces();
+
+        for (Class<?> anInterface : interfaces) {
+            if (anInterface.isAssignableFrom(StandardWindow.class))
+                return true;
+        }
+        return false;
+    }
+
+}
